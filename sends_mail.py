2228ea3ac4a9d565b1cbded16110ceede9432f1d
jessica stenning <jessicastenning@hotmail.co.uk>
2018-07-15 23:09:55 +0100

    Adds gitignore, refactors sends_mail

--- sends_mail.py
+++ sends_mail.py
@@ -1,5 +1,5 @@
-import payments
 from nameko.events import event_handler
+import payments
 import requests
 
 class SendsMail:
@@ -11,14 +11,23 @@ class SendsMail:
         print("payment received:", payload)
         return requests.post(
             "https://api.mailgun.net/v3/sandbox755e3e3d474a4b218e031f7213467c79.mailgun.org/messages",
-            auth=("api", "7f0ee52f26f4a4ef8328e1195556c961-8b7bf2f1-1a549565"),
-            data={"from": "jessicastenning@gmail.com",
+            auth=("api", "5ff5b93ffdea5568338776eaf366f060-8b7bf2f1-17f4b7c0"),
+            data = self.format_response_func(payload))
+
+    def format_response_func(self,payload):
+            return {
+                "from": "jessicalstenning@gmail.com",
                 "to": "jessicalstenning@gmail.com",
                 "subject": "Payment received",
-                "text":   ("Dear {}, "
-                "You have received a payment of {} {} "
-                "from {} ({}). "
-                "Yours, "
-                "Student.com ").format((payload['payee']['name']), (payload['payment']['amount']),
-                (payload['payment']['currency']), (payload['client']['name']),
-                (payload['client']['email']))})
+                "text": ("""
+                    Dear {},
+
+                    You have received a payment of {} {} from {} ({}).
+
+                    Yours,
+
+                    Student.com
+                    """).format(
+                    (payload['payee']['name']),(payload['payment']['amount']),
+                    (payload['payment']['currency']), (payload['client']['name']),
+                    (payload['client']['email']))}
--- test_sends_mail.py
+++ test_sends_mail.py
@@ -1,6 +1,2 @@
 from nameko.rpc import RpcProxy, rpc
 from nameko.testing.services import worker_factory
-
-def test_sends_mail():
-
-    service = worker_factory(SendsMail)
